5ca4e070ae5f7d73c5aca1a119999c7992dc3925
ramboo0524 <ramboo0524@126.com>
2019-12-24 15:18:20 +0800

    测试提交代码

--- app/src/main/java/com/yundao/ydwms/LoginAccountIPSettingActivity.java
+++ app/src/main/java/com/yundao/ydwms/LoginAccountIPSettingActivity.java
@@ -10,5 +10,6 @@ public class LoginAccountIPSettingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.activity_loginaccount_ipsetting);
+        System.out.println("hello");
     }
 }
